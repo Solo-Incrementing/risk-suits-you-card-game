29ac7ed43ecc1b9d70322a6d1a49d7ec2afe9c99
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2024-04-28 17:51:39 +0100

    Add tests.h header file for tests.cpp

---
+++ tests.h
@@ -0,0 +1,9 @@
+#ifndef TESTING_2024_04_28_17_45
+#define TESTING_2024_04_28_17_45
+
+int testGenerateRandomCardSuit(int testCount);
+int testGenerateRandomCardType(int testCount);
+int testGenerateCardNumber(int testCount);
+int testAllFunctions();
+
+#endif
