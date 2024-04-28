6dfc04ad5e50ddfefbe8c3f50ee8a42c5f0b1173
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2024-04-28 17:52:55 +0100

    Remove testing functions and add includes for main.h and tests.h to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 #include "Random.h"
+#include "main.h"
+#include "tests.h"
 
 #define DEBUG
 
@@ -52,106 +54,6 @@ char generateRandomCardType()
 int generateCardNumber()
 {
     return Random::get(2, 10);
-}
-
-/*
-* Test geenerateRandomCardSuit function
-* @param testCount - number of tests to run
-* @return 0 if all cases pass, 1 if a test fails
-*/
-int testGenerateRandomCardSuit(int testCount)
-{
-    for (int i{ 0 }; i < testCount; ++i)
-    {
-        char cardSuit{ generateRandomCardSuit() };
-
-        switch (cardSuit)
-        {
-        case 'C':
-        case 'S':
-        case 'D':
-        case 'H':
-            break;
-        default:
-            return 1;
-        }
-    }
-
-    return 0;
-}
-
-/*
-* Test geenerateRandomCardType function
-* @param testCount - number of tests to run
-* @return 0 if all cases pass, 1 if a test fails
-*/
-int testGenerateRandomCardType(int testCount)
-{
-    int numberCount{ 0 };
-    int faceCount{ 0 };
-    int aceCount{ 0 };
-    int otherCount{ 0 };
-
-    for (int i{ 0 }; i < testCount; ++i)
-    {
-        char cardType{ generateRandomCardType() };
-
-        switch (cardType)
-        {
-        case 'N':
-            ++numberCount;
-            break;
-        case 'F':
-            ++faceCount;
-            break;
-        case 'A':
-            ++aceCount;
-            break;
-        default:
-            ++otherCount;
-        }
-    }
-
-    //std::cout << "Number Count: " << numberCount << '\n';
-    //std::cout << "Face Count: " << faceCount << '\n';
-    //std::cout << "Ace Count: " << aceCount << '\n';
-    //std::cout << "Other Count: " << otherCount << '\n';
-
-    if (otherCount > 0) return 1;
-
-    return 0;
-}
-
-/*
-* Test generateCardNumber function
-* @param testCount - number of tests to run
-* @return 0 if all cases pass, test number if a test fails
-*/
-int testGenerateCardNumber(int testCount)
-{
-    for (int i{ 0 }; i < testCount; ++i)
-    {
-        int cardNumber{ generateCardNumber() };
-
-        if (cardNumber < 2) return 1;
-        if (cardNumber > 10) return 2;
-    }
-
-    return 0;
-}
-
-/*
-* Tests all functions
-* @param testCount - number of tests to run
-* @return 0 if all tests pass, otherwise test number that failed
-*/
-int testAllFunctions()
-{
-    if (testGenerateRandomCardSuit(100) != 0) return 1;
-    if (testGenerateRandomCardType(1300) != 0) return 2;
-    if (testGenerateCardNumber(100) != 0) return 3;
-
-    return 0;
 }
 
 int main()
